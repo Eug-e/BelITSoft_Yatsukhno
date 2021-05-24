c46d685ad47d90c6ce37e89d7f1c0d9c45bf1577
Eug_e <e7yatsukhno@gmail.com>
2021-05-25 01:24:54 +0300

    26/05/2021

--- Task C.py
+++ Task C.py
@@ -7,7 +7,6 @@
 from selenium import webdriver
 import unittest
 
-
 class Conversions(unittest.TestCase):
 
     @classmethod
@@ -16,14 +15,17 @@ class Conversions(unittest.TestCase):
         cls.driver.implicitly_wait(10)
         cls.driver.maximize_window()
 
+# Test for converting Celsius to Fahrenheit temperature
     def test_temperature(self):
         self.driver.get('https://www.metric-conversions.org/temperature/celsius-to-fahrenheit.html')
         self.driver.find_element_by_name("argumentConv").send_keys(10)
 
+# Test for converting meters to feet
     def test_length(self):
         self.driver.get('https://www.metric-conversions.org/length/meters-to-feet.htm')
         self.driver.find_element_by_name("argumentConv").send_keys(20)
 
+# Test for converting ounces to grams
     def test_weight(self):
         self.driver.get('https://www.metric-conversions.org/weight/ounces-to-grams.htm')
         self.driver.find_element_by_name("argumentConv").send_keys(30)
@@ -33,7 +35,6 @@ class Conversions(unittest.TestCase):
         cls.driver.close()
         cls.driver.quit()
         print('Completed!')
-
 
 if __name__ == '__main__':
     unittest.main()
